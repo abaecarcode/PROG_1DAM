7099bcbb1f054a262db78d47e1a133e9cd39d074
Alejandro Torres <atorhue617@g.educaand.es>
2024-10-22 20:17:49 +0200

    Primer programa en Java (siguiendo el libro Piensa en Java, pag69

---
+++ Proyecto1_Iniciacion_JAVA/src/HolaFecha.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class HolaFecha {
+    public static void main(String[] args) {
+        System.out.println("Hola, hoy es: ");
+        System.out.println(new Date());
+    }
+}
