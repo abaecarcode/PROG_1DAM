01b0d0a78c4e56589c188ee61ca470ba33c61e41
Torretas <atorhue617@g.educaand.es>
2024-11-06 13:09:39 +0100

    busquedas aleatorias y del tesores añadidos

--- Proyecto1_Iniciacion_JAVA/src/entrada_datos/EntradaAvanzada.java
+++ Proyecto1_Iniciacion_JAVA/src/entrada_datos/EntradaAvanzada.java
@@ -1,10 +1,22 @@
 package entrada_datos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class EntradaAvanzada {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        BufferedReader entrada1 = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Introduce una frase:");
+            String frase1 = entrada1.readLine(); // leerá toda la línea
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         // Leer una cadena completa (con espacios)
         System.out.println("Introduce una frase:");
--- Proyecto2_Metodos_y_funciones/src/bol01ej22/ej22.java
+++ Proyecto2_Metodos_y_funciones/src/bol01ej22/ej22.java
@@ -14,70 +14,140 @@ public class ej22 {
         unidades = num % 10;
         decenas = num / 10;
 
-        switch (decenas) {
-            case 0:
-                System.out.print("");
-                break;
-            case 1:
-                System.out.print("diez");
-                break;
-            case 2:
-                System.out.print("veinte");
-                break;
-            case 3:
-                System.out.print("treinta");
-                break;
-            case 4:
-                System.out.print("cuarenta");
-                break;
-            case 5:
-                System.out.print("cincuenta");
-                break;
-            case 6:
-                System.out.print("sesenta");
-                break;
-            case 7:
-                System.out.print("setenta");
-                break;
-            case 8:
-                System.out.print("ochenta");
-                break;
-            case 9:
-                System.out.print("noventa");
-                break;
-        }
-        System.out.print (" y ");
-        switch(unidades){
-            case 0:
-                System.out.println("");
-                break;
-            case 1:
-                System.out.println("uno");
-                break;
-            case 2:
-                System.out.println("dos");
-                break;
-            case 3:
-                System.out.println("tres");
-                break;
-            case 4:
-                System.out.println("cuatro");
-                break;
-            case 5:
-                System.out.println("cinco");
-                break;
-            case 6:
-                System.out.println("seis");
-                break;
-            case 7:
-                System.out.println("siete");
-                break;
-            case 8:
-                System.out.println("ocho");
-                break;
-            case 9:
-                System.out.println("nueve");
-                break;
+        // Caso especial para lo snumeros entre 11 y 15
+
+        if (num >= 11 && num <= 15) {
+            switch (num) {
+                case 11:
+                    System.out.println("once");
+                    break;
+                case 12:
+                    System.out.println("doce");
+                    break;
+                case 13:
+                    System.out.println("trece");
+                    break;
+                case 14:
+                    System.out.println("catorce");
+                    break;
+                case 15:
+                    System.out.println("quince");
+                    break;
+            }
+        } else {
+            switch (decenas) {
+                case 0:
+                    System.out.print("");
+                    break;
+                case 1:
+                    if (unidades == 0) {
+                        System.out.print("diez");
+                    } else {
+                        System.out.print("dieci");
+                    }
+                    break;
+                case 2:
+                    if (unidades == 0) {
+                        System.out.print("veinte");
+                    } else {
+                        System.out.print("veinti");
+                    }
+                    break;
+                case 3:
+                    System.out.print("treinta");
+                    break;
+                case 4:
+                    System.out.print("cuarenta");
+                    break;
+                case 5:
+                    System.out.print("cincuenta");
+                    break;
+                case 6:
+                    System.out.print("sesenta");
+                    break;
+                case 7:
+                    System.out.print("setenta");
+                    break;
+                case 8:
+                    System.out.print("ochenta");
+                    break;
+                case 9:
+                    System.out.print("noventa");
+                    break;
+            }
+
+            if (decenas > 2 && unidades != 0)
+                System.out.println(" y ");
+            if (decenas != 1 && decenas != 2) {
+                switch(unidades){
+                    case 0:
+                        System.out.println("");
+                        break;
+                    case 1:
+                        System.out.println("uno");
+                        break;
+                    case 2:
+                        System.out.println("dos");
+                        break;
+                    case 3:
+                        System.out.println("tres");
+                        break;
+                    case 4:
+                        System.out.println("cuatro");
+                        break;
+                    case 5:
+                        System.out.println("cinco");
+                        break;
+                    case 6:
+                        System.out.println("seis");
+                        break;
+                    case 7:
+                        System.out.println("siete");
+                        break;
+                    case 8:
+                        System.out.println("ocho");
+                        break;
+                    case 9:
+                        System.out.println("nueve");
+                        break;
+                }
+            } else {
+                if (decenas == 1 || decenas == 2) {
+                    switch(unidades){
+                        case 0:
+                            System.out.println("");
+                            break;
+                        case 1:
+                            System.out.println("uno");
+                            break;
+                        case 2:
+                            System.out.println("dos");
+                            break;
+                        case 3:
+                            System.out.println("tres");
+                            break;
+                        case 4:
+                            System.out.println("cuatro");
+                            break;
+                        case 5:
+                            System.out.println("cinco");
+                            break;
+                        case 6:
+                            System.out.println("seis");
+                            break;
+                        case 7:
+                            System.out.println("siete");
+                            break;
+                        case 8:
+                            System.out.println("ocho");
+                            break;
+                        case 9:
+                            System.out.println("nueve");
+                            break;
+                    }
+                }
+            }
+
         }
     }
 }
