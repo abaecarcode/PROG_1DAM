29aaa3455b6f0451a7e86f9acbe3dce8c10b3187
Alejandro Torres <atorhue617@g.educaand.es>
2024-11-27 11:42:57 +0100

    Reestructuración de la relacion de ejercicios. Se añaden ejercicio 6, 7 y 8 a la relacion 2 de POO para trabajar Herencia, Polimorfismo y Abstracción

--- U4_POO_clases/Ejercicios/Estudiante_3Ej1.java
+++ U4_POO_clases/Ejercicios/Estudiante_3Ej1.java
@@ -3,12 +3,12 @@ package Ejercicios;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Estudiante_2Ej1 {
+public class Estudiante_3Ej1 {
     private String nombre;
     private int edad;
     private String matricula;
 
-    public Estudiante_2Ej1(String nombre, int edad, String matricula) {
+    public Estudiante_3Ej1(String nombre, int edad, String matricula) {
         this.nombre = nombre;
         this.edad = edad;
         this.matricula = matricula;
@@ -44,7 +44,7 @@ public class Estudiante_2Ej1 {
         System.out.println("Matricula: " + getMatricula());
     }
 
-    public static void registrarEstudiante(ArrayList<Estudiante_2Ej1> estudiantes) {
+    public static void registrarEstudiante(ArrayList<Estudiante_3Ej1> estudiantes) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el nombre del estudiante: ");
         String nombre = sc.nextLine();
@@ -54,13 +54,13 @@ public class Estudiante_2Ej1 {
         System.out.println("Ingrese el matricula del estudiante: ");
         String matricula = sc.next();
 
-        Estudiante_2Ej1 nuevo_estudiante = new Estudiante_2Ej1(nombre, edad, matricula);
+        Estudiante_3Ej1 nuevo_estudiante = new Estudiante_3Ej1(nombre, edad, matricula);
         estudiantes.add(nuevo_estudiante);
         System.out.println("Estudiante registrado con éxito");
     }
 
     public static void main(String[] args) {
-        Estudiante_2Ej1 e1 = new Estudiante_2Ej1("Pepe", 17,"IAG1");
+        Estudiante_3Ej1 e1 = new Estudiante_3Ej1("Pepe", 17,"IAG1");
         String n, m;
         int e;
 
@@ -72,13 +72,13 @@ public class Estudiante_2Ej1 {
         sc.nextLine();
         System.out.println("Ingrese la matricula del estudiante: ");
         m = sc.nextLine();
-        Estudiante_2Ej1 e2 = new Estudiante_2Ej1(n,e,m);
+        Estudiante_3Ej1 e2 = new Estudiante_3Ej1(n,e,m);
         e1.mostrarInfo();
         e1.mostrarInfo2();
         System.out.println("Info del estudiante numero2 recogido por teclado:");
         e2.mostrarInfo();
 
-        ArrayList<Estudiante_2Ej1> estudiantes = new ArrayList<Estudiante_2Ej1>();
+        ArrayList<Estudiante_3Ej1> estudiantes = new ArrayList<Estudiante_3Ej1>();
         registrarEstudiante(estudiantes);
     }
 }
