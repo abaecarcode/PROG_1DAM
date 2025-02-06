48ae1a04d6f54683db3ded73479a9a19c75f7da9
Torretas <atorhue617@g.educaand.es>
2025-02-06 13:49:54 +0100

    Parking POO

---
+++ U4_POO_clases/EjPOO_05_Parking/ParkingException.java
@@ -0,0 +1,20 @@
+package EjPOO_05_Parking;
+
+public class ParkingException extends Exception {
+
+    private final String mensaje;
+    private final String matricula;
+
+    public ParkingException(String mensaje, String matricula) {
+        this.mensaje = mensaje;
+        this.matricula = matricula;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+}
